167bf0a8bd4d6dd5f89fc2a4b1f1a669261ec3d1
chumpa <chumpa@yandex.ru>
2019-08-28 20:13:09 +0400

    changed
    Signed-off-by: chumpa <chumpa@yandex.ru>

--- sf-toolset/camel/src/test/java/TestSfCamel.java
+++ sf-toolset/camel/src/test/java/TestSfCamel.java
@@ -8,10 +8,8 @@ import java.net.URI;
 public class TestSfCamel {
     @Test
     public void metadata() throws Exception {
-        SFHost sfHost = new SFHost("ooogruppakT2", new URI("https://api18preview.sapsf.com"))
-                .setCE("ilya.kuznetsov@sap.com", "1qaz#EDC");
-//        SFHost sfHost = new SFHost("companyId", new URI("https://api12preview.sapsf.com"))
-//                .setCE("login", "password");
+        SFHost sfHost = new SFHost("companyId", new URI("https://api12preview.sapsf.com"))
+                .setCE("login", "password");
         CEAPI ceapi = new CEAPI();
         SFCECamelHelper helper = new SFCECamelHelper(sfHost, ceapi);
         helper.describeEx();
